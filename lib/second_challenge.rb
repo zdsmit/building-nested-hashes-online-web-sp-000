31c8ca5d79cd636ae3786e67cfe22d15ac6865c7
Zach  Smith <zdsmith001@gmail.com>
2019-07-30 19:02:03 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,9 +1,9 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriarch => {},
-      :matriarch => {},
-      :hero => {},
+      :patriarch => {name: Lord Montague, age: 53},
+      :matriarch => {name: Lady Montague, age: 54},
+      :hero => {name: Romeo, age: 15, status: alive},
       :hero_friends => []
       },
     :capulet => {
