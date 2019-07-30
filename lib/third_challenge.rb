41df2e107c3121f52c56c50a3cc5ee81e1b9d0cf
Zach  Smith <zdsmith001@gmail.com>
2019-07-30 19:11:38 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -10,7 +10,7 @@ def third_challenge
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
-      :heroine_friends => [{name: "Steven", age: "30", attitude: "confused"}, {name: "Nurse", age: 44, attitude: "worried"}]
+      :heroine_friends => [{name: "Steven", age: "30", attitude: "confused"}, {name: "Nurse", age: "44", attitude: "worried"}]
       }
     }
 
