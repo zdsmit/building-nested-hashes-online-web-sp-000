b49dc9583d39feba7133ac89e4b559abd791c986
Zach  Smith <zdsmith001@gmail.com>
2019-07-30 19:05:21 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -3,14 +3,14 @@ def second_challenge
     :montague => {
       :patriarch => {name: Lord Montague, age: 53},
       :matriarch => {name: Lady Montague, age: 54},
-      :hero => {name: Romeo, age: 15, status: alive},
+      :hero => {name: Romeo, age: 15, status: Alive},
       :hero_friends => [{name: Benivolo, age: 17, attitude: worried}, {name: Mercutio, age: 18, attitude: hot-headed}]
       },
     :capulet => {
       :patriarch => {name: Lord Capulet, age: 50},
       :matriarch => {name: Lady Capulet, age: 51},
-      :heroine => {},
-      :heroine_friends => []
+      :heroine => {name: Juliet, age: 15, status: Alive},
+      :heroine_friends => [{name: Steven, age: 30, attitude: confused}, {name: Nurse, age: 44, attitude: worried}]
       }}
 
 
