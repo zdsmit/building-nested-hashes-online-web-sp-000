194f73abde1567714251f7c226e88f975d654de7
Zach  Smith <zdsmith001@gmail.com>
2019-07-30 19:00:00 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -5,7 +5,7 @@ def second_challenge
       :matriarch => {},
       :hero => {},
       :hero_friends => []
-      }, 
+      },
     :capulet => {
       :patriarch => {},
       :matriarch => {},
@@ -13,5 +13,5 @@ def second_challenge
       :heroine_friends => []
       }}
 
-  
+
 end
