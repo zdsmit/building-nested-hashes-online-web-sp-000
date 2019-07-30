5f294870dea538993966a5a7946033a0a0af3f91
Zach  Smith <zdsmith001@gmail.com>
2019-07-30 19:10:39 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,16 +1,16 @@
 def third_challenge
   epic_tragedy = {
     :montague => {
-      :patriarch => {name:Lord Montague, age:53},
-      :matriarch => {name: Lady Montague, age: 54},
-      :hero => {name: Romeo, age: 15, status: Alive},
-      :hero_friends => [{name: Benivolo, age: 17, attitude: worried}, {name: Mercutio, age: 18, attitude: hot-headed}]
+      :patriarch => {name: "Lord Montague", age: 53},
+      :matriarch => {name: "Lady Montague", age: 54},
+      :hero => {name: "Romeo", age: 15, status: "alive"},
+      :hero_friends => [{name: "Benivolo", age: 17, attitude: "worried"}, {name: "Mercutio", age: 18, attitude: "hot-headed"}]
       },
     :capulet => {
-      :patriarch => {name: Lord Capulet, age: 50},
-      :matriarch => {name: Lady Capulet, age: 51},
-      :heroine => {name: Juliet, age: 15, status: Alive},
-      :heroine_friends => [{name: Steven, age: 30, attitude: confused}, {name: Nurse, age: 44, attitude: worried}]
+      :patriarch => {name: "Lord Capulet", age: 50},
+      :matriarch => {name: "Lady Capulet", age: 51},
+      :heroine => {name: "Juliet", age: 15, status: "alive"},
+      :heroine_friends => [{name: "Steven", age: 30, attitude: "confused"}, {name: "Nurse", age: 44, attitude: "worried"}]
       }
     }
 
